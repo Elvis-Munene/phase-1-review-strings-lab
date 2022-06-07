143fc444a70dcbf7ef5bfba16a6ae592f3f4f4d8
Elvis-Munene <gitau.elvis@student.moringaschool.com>
2022-06-07 04:35:42 +0300

    commit tree 463cffb82f10fa982fbdbe651d4abf799f1ab826

--- index.js
+++ index.js
@@ -1,1 +1,16 @@
 // Write your code in this file!
+const currentUser = "ELVIS";
+
+const welcomeMessage = `Welcome to Flatbook, ${currentUser}!`;
+ console.log(welcomeMessage);
+
+
+ const excitedWelcomeMessage = `WELCOME TO FLATBOOK, '${currentUser}'!`;
+
+ console.log(excitedWelcomeMessage);
+
+ const shortGreeting = "Welcome, E!";
+
+ console.log(shortGreeting);
+
+ 
